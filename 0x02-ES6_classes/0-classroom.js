e86178177fc888b23593b819ec54e327ee4e6198
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-08-08 23:16:42 +0100

    manual typecast removed

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -6,6 +6,6 @@ and assigned to _maxStudentsSize
 */
 export default class ClassRoom {
     constructor(maxStudentsSize) {
-        this._maxStudentsSize = Number(maxStudentsSize);
+        this._maxStudentsSize = maxStudentsSize;
     }
 }
