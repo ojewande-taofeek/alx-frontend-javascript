82a9ec2410cb37e0c6a4f6f9c9e54be6a86f6e2c
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-07 12:07:05 +0100

    a function that returns an array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,4 @@
+// function named getListStudents that returns an array of objects.
 export default function getListStudents() {
   return [{id: 1, firstName: "Guillaume", location: "San Francisco"},
     {id: 2, firstName: "James", location: "Columbia"},
