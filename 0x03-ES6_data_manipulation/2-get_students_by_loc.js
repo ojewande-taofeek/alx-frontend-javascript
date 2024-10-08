717e0fc9ca2cd8207fe67b484ccec2c153b0130c
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-08 14:17:40 +0100

    array filter method used - lint fixed

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,6 @@
-// function getStudentsByLocation that returns an array of objects who are located in a specific city.
+// function getStudentsByLocation that returns an array of objects
+// who are located in a specific city.
 export default function getStudentsByLocation(students, city) {
-  const cityStudents = students.filter((student) => student.location === city)
+  const cityStudents = students.filter((student) => student.location === city);
   return cityStudents;
 }
