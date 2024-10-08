de1652739d52ab17c718bd8549285d20a22e647f
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-08 13:41:55 +0100

    typed array using DataView

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  if (length > position) {
+    const view = new DataView(buffer, 0);
+    view.setInt8(position, value);
+    return view;
+} else {
+    throw Error('Position outside range');
+}
+}
