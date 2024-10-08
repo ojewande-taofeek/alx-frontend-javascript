62b29d9fb694fb5e7c1bdb054aefe2029d3f9f81
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-08 14:24:26 +0100

    map, filter and ficnd used

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.filter((student) => student.location === city )
+  return students.filter((student) => student.location === city)
     .map((student) => {
-        const exactStudent = newGrades.find((newG) => student.id === newG.studentId);
-        if (exactStudent && exactStudent.grade) {
-            student.grade = exactStudent.grade;
-        } else {
-            student.grade = 'N/A';
-        }
-        return student;
+      const exactStudent = newGrades.find((newG) => student.id === newG.studentId);
+      if (exactStudent && exactStudent.grade) {
+        student.grade = exactStudent.grade;
+      } else {
+        student.grade = 'N/A';
+      }
+      return student;
     });
 }
