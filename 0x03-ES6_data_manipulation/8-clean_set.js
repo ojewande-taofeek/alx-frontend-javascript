a418f44d160f9e418cc957eebe14e127f9bc1913
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-08 20:29:13 +0100

    array methods used

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
+  if (!startString || typeof startString !== 'string' || typeof set !== 'object') {
     return '';
   }
   // converted the set to an array
