8830ff8e8314e91f6138ce06edde9932b4c85430
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-08 16:57:54 +0100

    set data structure - has method - lint fixed

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,7 @@
 export default function hasValuesFromArray(set, array) {
   for (const element of array) {
     if (set.has(element) === false) {
-        return false
+      return false;
     }
   }
   return true;
