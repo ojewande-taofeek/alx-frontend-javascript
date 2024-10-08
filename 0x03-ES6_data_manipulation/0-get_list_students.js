5210b7a049fd97cf8d06755ad433ef5f50ecd95d
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-08 13:51:40 +0100

    an array of three objects created

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 // function named getListStudents that returns an array of objects.
 export default function getListStudents() {
-  const arr_ay = [{id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+  return [
+    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
     {id: 2, firstName: 'James', location: 'Columbia'},
     {id: 5, firstName: 'Serena', location: 'San Francisco'}
   ];
-  return arr_ay;
 }
