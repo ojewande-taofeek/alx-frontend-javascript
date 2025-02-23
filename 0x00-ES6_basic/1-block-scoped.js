fc7a05cfef2fdacf93e5c2089e42d7e50d9e7a49
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2025-02-23 16:22:08 +0100

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
+    const task = false;
     const task2 = false;
   }
 
