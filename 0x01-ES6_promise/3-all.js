8e27ed078a7b1705145d6788cca05d33e6dd089f
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2025-02-26 12:29:08 +0100

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const { body } = results[0];
       const { firstName } = results[1];
