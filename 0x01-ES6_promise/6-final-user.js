1c7b316d23b1773e93e81a334bce438f2201df44
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2025-02-26 14:50:38 +0100

    allsettled method

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,14 @@
 import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
-export default function handleProfileSignup(firtName, lastName, filename) {
-  const userPhone = uploadPhoto(filename);
-  const userName = signUpUser(firtName, lastName);
-  return Promise.allSettled([userPhone, userName])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
-    })));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+  return Promise.allSettled([promise1, promise2])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason.toString() };
+    }));
 }
