352c1cfec727125bdd42ae873436fbd9977fb536
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2025-03-02 21:13:36 +0100

    Let's build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,10 @@
+;
+// Example 
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
